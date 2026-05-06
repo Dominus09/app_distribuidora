1d08630f84c0e68a47790cb9c05b256b5af7aaa5
Dominus09 <katoonxd@gmail.com>
2026-05-05 23:32:55 -0400

    nombre del avance

--- app_distribuidora/android/app/build.gradle.kts
+++ app_distribuidora/android/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "cl.quillotana.ops"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 4
+        versionName = "1.0.4"
 
         manifestPlaceholders["GOOGLE_MAPS_API_KEY"] = googleMapsApiKey
     }
